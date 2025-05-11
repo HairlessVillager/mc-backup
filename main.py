e881e521f08b019ddeda0f94144919a11634d688
HairlessVillager <hairlessvillager@foxmail.com>
2025-05-11 15:13:39 +0800

    fix

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import time
 from datetime import datetime
 import mcrcon
 
-source_folder = ".venv"
-backup_base_path = "venv-backup"
+source_folder = ""
+backup_base_path = ""
+subfolders_to_backup = ["world", "world_nether", "world_the_end"]
 
 
 def create_backup():
@@ -21,13 +22,13 @@ def create_backup():
             response = client.command("save-all flush")
             logger.info(f"rcon response: {response}")
             t1 = time.time()
-            client.command(f"say save-all...flushed in {t1-t0:.3f}")
-            logger.info(f"save-all...flushed in {t1-t0:.3f}")
+            client.command(f"say save-all...flushed in {t1 - t0:.3f}")
+            logger.info(f"save-all...flushed in {t1 - t0:.3f}")
     except ConnectionRefusedError as err:
         logger.warning("An error occurs when 'save-all':")
         logger.exception(err)
         logger.warning("But backuping continues.")
-        
+
     now = datetime.now()
 
     backup_folder_name = now.strftime("%Y%m%d_%H%M%S")
@@ -37,10 +38,18 @@ def create_backup():
         os.makedirs(backup_folder_path)
 
     try:
-        shutil.copytree(
-            source_folder,
-            os.path.join(backup_folder_path, os.path.basename(source_folder)),
-        )
+        for subfolder in subfolders_to_backup:
+            subfolder_path = os.path.join(source_folder, subfolder)
+            if os.path.isdir(subfolder_path):
+                destination_path = os.path.join(backup_folder_path, subfolder)
+                shutil.copytree(subfolder_path, destination_path)
+                logger.info(
+                    f"Backup created for subfolder {subfolder} at {destination_path}"
+                )
+            else:
+                logger.warning(
+                    f"Subfolder {subfolder} does not exist in source folder."
+                )
         logger.info(f"Backup created at {backup_folder_path}")
     except Exception as e:
         logger.error(f"Failed to create backup: {e}")
@@ -76,11 +85,20 @@ def cleanup_old_backups(daily_backup_retention: int):
 
 
 def main():
+    if not all(bool(e) for e in [source_folder, backup_base_path]):
+        raise Exception("'source_folder' and 'backup_base_path' cannot be empty")
+
     if not os.path.exists(backup_base_path):
         os.makedirs(backup_base_path)
 
     log_file_path = os.path.join(backup_base_path, "backup_scheduler.log")
-    logger.add(log_file_path, rotation="100 MB", compression="zip", backtrace=True, diagnose=True)
+    logger.add(
+        log_file_path,
+        rotation="100 MB",
+        compression="zip",
+        backtrace=True,
+        diagnose=True,
+    )
 
     schedule.every().minute.at(":00").do(create_backup)
     schedule.every().day.at("00:02").do(lambda: cleanup_old_backups(1))
@@ -92,6 +110,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    create_backup()
-    # cleanup_old_backups(1)
+    main()
